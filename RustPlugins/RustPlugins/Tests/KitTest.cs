f202577b1094d4ac88cd79023d4b0dff3035f2e8
Dennis Ris <dennis.ris@isatis-health.nl>
2015-02-20 11:17:58 +0100

    Added AutoMoq nuget

--- RustPlugins/RustPlugins/Tests/KitTest.cs
+++ RustPlugins/RustPlugins/Tests/KitTest.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Oxide.Plugins;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
 
 namespace RustPlugins.Tests
 {
     [TestFixture]
     public class KitTest
     {
+        private IFixture _fixture { get; set; }
+        private Kits _sut;
+
         [SetUp]
         public void Setup()
         {
-            
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            _sut = _fixture.CreateAnonymous<Kits>();
         }
 
         [Test]
         public void Test()
         {
-            
+            //_sut.GiveItem();
         }
 
     }
