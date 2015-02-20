68c99f029ab0b4743005f8bf3f6f54562ad0ae75
Dennis Ris <dennis.ris@isatis-health.nl>
2015-02-20 11:02:54 +0100

    Added unittest references NUnit and Moq

---
+++ RustPlugins/RustPlugins/Tests/KitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RustPlugins.Tests
+{
+    [TestFixture]
+    public class KitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            
+        }
+
+    }
+}
